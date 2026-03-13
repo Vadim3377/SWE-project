5606c605bf967336f5079eac3928d4993dd480d1
Sulav <sulavp@SulavLaptop>
2026-03-13 19:42:50 +0000

    Added comments for sirport, aircraft and runways

--- backend/aircraft.py
+++ backend/aircraft.py
@@ -5,22 +5,28 @@ from typing import Optional
 # from .SimulationEngine import EmergencyType
 # Defined here to avoid circular import from SimulationEngine
 
+"""
+This is the dataclass for the emergencies that an aircraft can have
+We made them to be boolean because an aircraft can either have any one of these emergencies at a time or none at all
+"""
 @dataclass
 class EmergencyType:
     mechanical_failure: bool = False
     passenger_illness: bool = False
     fuel_emergency: bool = False
 
+"""
+- The Aircraft class is built to contain information about an airplane object
+- The aircraft type attribute can either be INBOUND or OUTBOUND
+- EmergencyType is CREATED by SimulationEngine and injected here.
+- The origin attribute of the aircraft depends on the flight type. if it's outbound, the origin is another randomly generated airport and if it's outbound, the origin is the airport itself 
+"""
 class Aircraft:
-    """
-    SimTime = int minutes.
-    EmergencyType is CREATED by SimulationEngine and injected here.
-    """
 
     def __init__(self, aircraft_id: str, flight_type: str, scheduledTime: int, fuelRemaining: int,*, emergency: Optional[EmergencyType] = None, altitude: int = 0, enteredHoldingAt: Optional[int] = None, joinedTakeoffQueueAt: Optional[int] = None):
         
         self.id = aircraft_id
-        self.type = flight_type #a string that will either be INBOUND or OUTBOUND
+        self.type = flight_type 
         self.scheduledTime = int(scheduledTime)
         self.fuelRemaining = int(fuelRemaining)
         self.altitude = altitude
@@ -28,8 +34,6 @@ class Aircraft:
         
         self.enteredHoldingAt = enteredHoldingAt
         self.joinedTakeoffQueueAt = joinedTakeoffQueueAt
-
-        # Cosmetic / UI fields
         icao_code = [
             "Boeing ", "Airbus ", "RYANAIR ", "Speedbird ", "Emirates ",
             "EASY ", "Oceanic ", "Virgin ", "Delta ", "United "
@@ -45,27 +49,41 @@ class Aircraft:
             self.origin = "SIMULATED_AIRPORT"
             self.destination = self._rand_airport()
 
+    """
+    - The method below is used to randomly generate 3 letters for airport codes for origin/destination
+    - This is done by randomly selecting 3 ASCII characters 
+    """
     @staticmethod
     def _rand_airport() -> str:
         return "".join(random.choice(string.ascii_uppercase) for _ in range(3))
 
+    """
+    - The method below checks if the aircraft has an emergency. If the emergency attribute is not declared, then there's no emergency so return false
+    - If the emergency attribute is declared then return true
+    """
     def isEmergency(self) -> bool:
         e = self.emergency
         if e is None:
             return False
         return e.mechanical_failure or e.passenger_illness or e.fuel_emergency 
 
+    """
+    - The following method below is called to decrement the remaining fuel by a certain amount for every tick for the aircraft.
+    - This is needed so that the aircraft will actually be "burning" fuel as the simulation progresses
+    """
     def consumeFuel(self, amount: int) -> None:
         self.fuelRemaining = max(0, self.fuelRemaining - int(amount))
-
+    
+    """
+    - This method below checks if the aircraft should be assigned a priority by  checking if it has an emergency (using a method expplained above)
+    - If there is an emergency, we return 0 so that when we add it to the holding queue, it will prioritise the aircraft with 0 in their tuple since it will be the lowest
+    - Lower value means higher priority so that emergency aircraft must always come first.
+    """
     def priority(self, time: int) -> int:
-        """
-        Lower value = higher priority.
-        Emergency aircraft must always come first.
-        But change if you want spectrum
-        Vadim
-        """
         return 0 if self.isEmergency() else 1
 
+    """
+    - This is a getter method which just returns the fuel remaining
+    """
     def getFuel(self) -> int:
         return self.fuelRemaining
--- backend/runway.py
+++ backend/runway.py
@@ -1,38 +1,55 @@
-import random
+import random #Needed to assign length of runway and the bearings
 
+"""
+- The Runway class is created for the variable number of runways that our simulation may have. 
+- Methods are available so we can check if we can land/takeoff on the runway, if the runway is available or if we want to assign an aircraft to a runway
+- Initially, we set up the runways so that they are all free and currently have no aircraft
+"""
 class Runway:
     def __init__(self, runway_id, runway_mode) -> None:
         self.id = runway_id
-        self.mode = runway_mode              # capability 
+        self.mode = runway_mode              # Represents runway capability 
         self.status = "AVAILABLE"
         self.occupancy = "FREE"
-        self.currentOperation = None         # "LANDING" or "TAKEOFF"
+        self.currentOperation = None         # Can be "LANDING" or "TAKEOFF" or "MIXED"
         self.occupiedUntil = 0
         self.currentAircraft = None
         self.length = random.randint(2000,4000)
         self.bearing = random.randint(1,36)
 
+    """
+    - Method description: Checks if the runway is available by checking its occupancy and its status
+    """
     def isAvailable(self):
         return self.occupancy == "FREE" and self.status == "AVAILABLE"
 
+    """
+    - Assigns an aircraft to a runway and changes the occupancy to being OCCUPIED
+    - It also stores how long the aircraft will be occupying the runway for.
+    """
     def assign(self, aircraft, operationMode="LANDING", time: int = 0, duration: int = 1) -> None:
         self.currentAircraft = aircraft
         self.currentOperation = operationMode
         self.occupiedUntil = time + duration
         self.occupancy = "OCCUPIED"
 
-    # Checks if a runway is available for a plane to land on it.
+    """
+    - The method below checks if a runway is available for a plane to land on it.
+    """
     def canLand(self) -> bool:
-        # Fixed typo: self.Mode -> self.mode
         return self.currentAircraft == None and ((self.mode == "MIXED") or (self.mode == "LANDING"))
 
-    # Checks if a runway is available for a plane to take off from it.
+    """
+    - The method below checks if a runway is available for a plane to take off from it.
+    """
     def canTakeOff(self) -> bool:
         # Fixed typo: self.Mode -> self.mode
         return self.currentAircraft == None and ((self.mode == "MIXED") or (self.mode == "TAKEOFF"))
 
-    #turns bearing into a string with the correct bearing format for UI output.
-    def getBearingString(self) -> str: # Fixed return type: bool -> str
+    """
+    - Method description: Turns the bearing into a string with the correct bearing format for UI output.
+    """
+    def getBearingString(self) -> str:
         if self.bearing <= 9:
             return str("0" + str(self.bearing))
         else:
